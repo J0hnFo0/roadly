0dc7cd103f77da0d4e1a822fbe525eeeeafc90ef
JensErhardt <jenserhardt@gmail.com>
2019-11-13 20:49:50 +0100

    Added Add index.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CustomerIndex from './customer/Index';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {this.renderContent()}
+      </div>
+    );
+  }
+
+  renderContent() {
+    return (
+      <Switch>
+        <Route path="/" render={this.renderCustomers} />
+      </Switch>
+    );
+  }
+
+  renderCustomers() {
+    return (
+      <CustomerIndex />
+    );
+  }
 }
 
 export default App;
