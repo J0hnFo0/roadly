7c7af133215d9130f392011fb102b2e11f5dde55
Jens Erhardt <j.erhardt@lum-gmbh.de>
2019-11-14 15:49:58 +0100

    Extended CustomerForm with fields corresponding to model, not finished.

--- client/src/components/customer/Form.jsx
+++ client/src/components/customer/Form.jsx
@@ -7,6 +7,43 @@ class CustomerForm extends React.PureComponent {
 
     return (
       <form onSubmit={props.handleSubmit} >
+
+        <div className="form-row">
+          <div className='form-group col-md-5'>
+            <label htmlFor='nameFirst'>Firmenname</label>
+            <input
+              type='text'
+              className='form-control'
+              id='nameFirst'
+              value="TODO"
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+          <div className='form-group col-md-5'>
+            <label htmlFor='nameFirst'>Kundennummer</label>
+            <input
+              type='text'
+              className='form-control'
+              id='nameFirst'
+              value="TODO"
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+          <div className='form-group col-md-2'>
+            <label htmlFor='number'>TAV-Nr</label>
+            <input
+              type='number'
+              className='form-control'
+              id='number'
+              value={props.number}
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+        </div>
+
         <div className='form-row'>
           <div className='form-group col-md-5'>
             <label htmlFor='nameFirst'>Vorname</label>
@@ -30,21 +67,8 @@ class CustomerForm extends React.PureComponent {
               required={true}
             />
           </div>
-          <div className='form-group col-md-2'>
-            <label htmlFor='sex'>Geschlecht</label>
-            <select
-              id='sex'
-              className='form-control'
-              value={props.sex}
-              onChange={props.handleChange}
-              required={true}
-            >
-              <option value=''>Bitte wählen..</option>
-              <option value={1}>Weiblich</option>
-              <option value={2}>Männlich</option>
-            </select>
-          </div>
         </div>
+
         <div className='form-row'>
           <div className='form-group col-md-10'>
             <label htmlFor='street'>Straße</label>
@@ -70,17 +94,6 @@ class CustomerForm extends React.PureComponent {
           </div>
         </div>
         <div className='form-row'>
-          <div className='form-group col-md-6'>
-            <label htmlFor='city'>Stadt</label>
-            <input
-              type='text'
-              className='form-control'
-              id='city'
-              value={props.city}
-              onChange={props.handleChange}
-              required={true}
-            />
-          </div>
           <div className='form-group col-md-2'>
             <label htmlFor='zipcode'>Postleitzahl</label>
             <input
@@ -92,13 +105,73 @@ class CustomerForm extends React.PureComponent {
               required={true}
             />
           </div>
+          <div className='form-group col-md-6'>
+            <label htmlFor='city'>Stadt</label>
+            <input
+              type='text'
+              className='form-control'
+              id='city'
+              value={props.city}
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
         </div>
-        <button 
-          type='submit' 
+
+        <div className="form-row">
+          <div className='form-group col-md-2'>
+            <label htmlFor='number'>Grubengröße</label>
+            <input
+              type='number'
+              className='form-control'
+              id='number'
+              value={props.number}
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+
+          <div className='form-group col-md-2'>
+            <label htmlFor='number'>Fahrzeug</label>
+            <select className="form-control">
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+
+          <div className='form-group col-md-2'>
+            <label htmlFor='number'>Startdatum</label>
+            <input
+              type='date'
+              className='form-control'
+              id='number'
+              value={props.number}
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+
+          <div className='form-group col-md-2'>
+            <label htmlFor='number'>Intervall</label>
+            <input
+              type='number'
+              className='form-control'
+              id='number'
+              value={props.number}
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+        </div>
+
+        <button
+          type='submit'
           className='btn btn-primary mr-2'
         >Übernehmen
         </button>
-        <Link 
+        <Link
           to='/'
           className='btn btn-secondary mr-2'>
           Abbrechen
@@ -110,7 +183,7 @@ class CustomerForm extends React.PureComponent {
 
   renderDeleteButton() {
     const id = this.props.id;
-    
+
     if (!id) {
       return;
     }
