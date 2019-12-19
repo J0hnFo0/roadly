b8c56f506941b03632fa1db5a916acaa6aaeada9
Jens Erhardt <jenserhardt@gmail.com>
2019-12-19 13:56:12 +0100

    Removed required attribute from company field. Extracted fields into render props.

--- client/src/components/customer/Form.jsx
+++ client/src/components/customer/Form.jsx
@@ -9,10 +9,21 @@ class CustomerForm extends React.PureComponent {
 
     return (
       <form onSubmit={props.handleSubmit} >
+        {this.renderMasterData()}
+        {this.renderAdress()}
+        {this.renderPickUp()}
+        {this.renderButtons()}
+      </form>
+    );
+  }
 
+  renderMasterData() {
+    const props = this.props;
+
+    return (
+      <React.Fragment>
         <h4>Stammdaten</h4>
         <hr />
-
         <div className="form-row">
           <div className='form-group col-md-6'>
             <label htmlFor='customerNumber'>Kundennummer</label>
@@ -38,12 +49,10 @@ class CustomerForm extends React.PureComponent {
               className='form-control'
               value={props.company}
               onChange={props.handleChange}
-              required={true}
             />
 
           </div>
         </div>
-
         <div className='form-row'>
           <div className='form-group col-md-6'>
             <label htmlFor='nameFirst'>Vorname</label>
@@ -69,73 +78,63 @@ class CustomerForm extends React.PureComponent {
             />
           </div>
         </div>
+      </React.Fragment>
+    );
+  }
 
-        <div className="form-row">
-          <div className="col-md-6  ">
-            <h4>Adresse</h4>
-            <hr />
+  renderAdress() {
+    const props = this.props;
 
-            <div className='form-row'>
-              <div className='form-group col-md-8'>
-                <label htmlFor='street'>Straße</label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='street'
-                  value={props.street}
-                  onChange={props.handleChange}
-                  required={true}
-                />
-              </div>
+    return (
+      <div className="form-row">
+        <div className="col-md-6  ">
+          <h4>Adresse</h4>
+          <hr />
 
-              <div className='form-group col-md-2'>
-                <label htmlFor='number'>Hausnummer</label>
-                <input
-                  type='number'
-                  className='form-control'
-                  id='number'
-                  value={props.number}
-                  onChange={props.handleChange}
-                  required={true}
-                />
-              </div>
+          <div className='form-row'>
+            <div className='form-group col-md-8'>
+              <label htmlFor='street'>Straße</label>
+              <input
+                type='text'
+                className='form-control'
+                id='street'
+                value={props.street}
+                onChange={props.handleChange}
+                required={true}
+              />
             </div>
-            <div className='form-row'>
-              <div className='form-group col-md-4'>
-                <label htmlFor='zipcode'>Postleitzahl</label>
-                <input
-                  type='number'
-                  className='form-control'
-                  id='zipcode'
-                  value={props.zipcode}
-                  onChange={props.handleChange}
-                  required={true}
-                />
-              </div>
-              <div className='form-group col-md-6'>
-                <label htmlFor='city'>Stadt</label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='city'
-                  value={props.city}
-                  onChange={props.handleChange}
-                  required={true}
-                />
-              </div>
-            </div>
-          </div>
 
-          <div className="col-md-6">
-            <h4>TAV</h4>
-            <hr />
-            <div className='form-group'>
-              <label htmlFor='tavArea'>TAV-Gebiet</label>
+            <div className='form-group col-md-2'>
+              <label htmlFor='number'>Hausnummer</label>
               <input
                 type='number'
                 className='form-control'
-                id='tavArea'
-                value={props.tavArea}
+                id='number'
+                value={props.number}
+                onChange={props.handleChange}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className='form-row'>
+            <div className='form-group col-md-4'>
+              <label htmlFor='zipcode'>Postleitzahl</label>
+              <input
+                type='number'
+                className='form-control'
+                id='zipcode'
+                value={props.zipcode}
+                onChange={props.handleChange}
+                required={true}
+              />
+            </div>
+            <div className='form-group col-md-6'>
+              <label htmlFor='city'>Stadt</label>
+              <input
+                type='text'
+                className='form-control'
+                id='city'
+                value={props.city}
                 onChange={props.handleChange}
                 required={true}
               />
@@ -143,84 +142,145 @@ class CustomerForm extends React.PureComponent {
           </div>
         </div>
 
-        <div className="form-row">
-          <div className="col-md-6">
-            <h4>Abholung</h4>
-            <hr />
+        <div className="col-md-6">
+          <h4>TAV</h4>
+          <hr />
+          <div className='form-group'>
+            <label htmlFor='tavArea'>TAV-Gebiet</label>
+            <input
+              type='number'
+              className='form-control'
+              id='tavArea'
+              value={props.tavArea}
+              onChange={props.handleChange}
+              required={true}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-            <div className="form-row">
-              <div className='form-group col-md-6'>
-                <label htmlFor='startDate'>Startdatum</label>
-                <DatePicker
-                  date={props.startDate}
-                  onChange={props.handleDate}
-                />
-              </div>
-              <div className='form-group col-md-6'>
-                <label htmlFor='pitSize'>Grubengröße</label>
-                <div className="input-group">
-                  <input
-                    type='number'
-                    className='form-control'
-                    id='pitSize'
-                    value={props.pitSize}
-                    onChange={props.handleChange}
-                    required={true}
-                  />
-                  <div class="input-group-append">
-                    <span class="input-group-text">m³</span>
-                  </div>
-                </div>
-              </div>
+  renderPickUp() {
+    const props = this.props;
+
+    return (
+      <div className="form-row">
+        <div className="col-md-6">
+          <h4>Abholung</h4>
+          <hr />
+          <div className="form-row">
+            <div className='form-group col-md-6'>
+              <label htmlFor='startDate'>Startdatum</label>
+              <DatePicker
+                date={props.startDate}
+                onChange={props.handleDate}
+              />
             </div>
-
-            <div className="form-row">
-              <div className='form-group col-md-6'>
-                <label htmlFor='interval'>Intervall</label>
+            <div className='form-group col-md-6'>
+              <label htmlFor='pitSize'>Grubengröße</label>
+              <div className="input-group">
                 <input
                   type='number'
                   className='form-control'
-                  id='interval'
-                  value={props.interval}
+                  id='pitSize'
+                  value={props.pitSize}
                   onChange={props.handleChange}
                   required={true}
                 />
+                <div class="input-group-append">
+                  <span class="input-group-text">m³</span>
+                </div>
               </div>
-              {this.renderCars()}
             </div>
           </div>
-
-          <div className="col-md-6">
-            <h4>Notizen</h4>
-            <hr />
-
-            <div class="form-group">
-              <label for="notes">Notizen</label>
-              <textarea
-                id="notes"
-                class="form-control"
-                rows="4"
+          <div className="form-row">
+            <div className='form-group col-md-6'>
+              <label htmlFor='interval'>Intervall</label>
+              <input
+                type='number'
+                className='form-control'
+                id='interval'
+                value={props.interval}
                 onChange={props.handleChange}
-                value={props.notes}
+                required={true}
               />
             </div>
-
+            {this.renderCars()}
           </div>
-
         </div>
+        <div className="col-md-6">
+          {this.renderNotes()}
+        </div>
+      </div>
+    );
+  }
 
+  renderCars() {
+    if (!this.props.cars) {
+      return;
+    }
+    /* Uncomment when we have cars */
+    //const cars = this.props.cars;
+    const cars = []
+    const options = cars.map((x, i) =>
+      <option value={i}>{cars.name}</option>
+    )
+
+    return (
+      <div className='form-group col-md-6'>
+        <label htmlFor='car'>Fahrzeug</label>
+        <select
+          id="car"
+          className="form-control"
+          onChange={this.props.handleChange}
+          value={this.props.car}
+        >
+          {options}
+        </select>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    const { handleChange, notes } = this.props;
+
+    return (
+      <React.Fragment>
+        <h4>Notizen</h4>
+        <hr />
+        <div class="form-group">
+          <label for="notes">Notizen</label>
+          <textarea
+            id="notes"
+            class="form-control"
+            rows="4"
+            onChange={handleChange}
+            value={notes}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  renderButtons() {
+    return (
+      <React.Fragment>
         <button
           type='submit'
           className='btn btn-primary mr-2'
-        >Übernehmen
+        >
+          Übernehmen
         </button>
         <Link
           to='/'
-          className='btn btn-secondary mr-2'>
+          className='btn btn-secondary mr-2'
+        >
           Abbrechen
-        </Link>
+          </Link>
         {this.renderDeleteButton()}
-      </form>
+      </React.Fragment>
+
     );
   }
 
@@ -240,32 +300,8 @@ class CustomerForm extends React.PureComponent {
       </button>
     )
   }
-  // Uncomment when we have cars
-  renderCars() {
-    if (!this.props.cars) {
-      return;
-    }
 
-    //const cars = this.props.cars;
-    const cars = []
-    const options = cars.map((x,i) => 
-    <option value={i}>{cars.name}</option> 
-    )
 
-    return (
-      <div className='form-group col-md-6'>
-        <label htmlFor='car'>Fahrzeug</label>
-        <select
-          id="car"
-          className="form-control"
-          onChange={this.props.handleChange}
-          value={this.props.car}
-        >
-          {options}
-        </select>
-      </div>
-    );
-  }
 }
 
 export default CustomerForm;
